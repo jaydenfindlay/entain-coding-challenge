253a0a5e108d559f3dcb106b70613cb2a1576b7c
Jayden Findlay <jayden.findlay@outlook.com>
2026-03-27 16:17:16 +1000

    Finalising task 3 - status now updates on init and query to whether the race is open or closed

--- racing/db/queries.go
+++ racing/db/queries.go
@@ -1,7 +1,8 @@
 package db
 
 const (
-	racesList = "list"
+	racesList         = "list"
+	racesUpdateStatus = "update_status"
 )
 
 func getRaceQueries() map[string]string {
@@ -13,8 +14,21 @@ func getRaceQueries() map[string]string {
 				name, 
 				number, 
 				visible, 
-				advertised_start_time 
+				advertised_start_time,
+				status
 			FROM races
+		`,
+	}
+}
+
+func updateStatusQueries() map[string]string {
+	return map[string]string{
+		racesUpdateStatus: `
+			UPDATE races
+			SET status = CASE
+				WHEN advertised_start_time < datetime('now') THEN 'CLOSED'
+				ELSE 'OPEN'
+			END
 		`,
 	}
 }
